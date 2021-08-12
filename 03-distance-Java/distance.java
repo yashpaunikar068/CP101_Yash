db338a456ef790e0524615b20a1ac98ab5142dd5
Yash Paunikar <ypaunikar@gmail.com>
2021-08-12 11:18:50 +0530

    done

--- 03-distance-Java/distance.java
+++ 03-distance-Java/distance.java
@@ -6,6 +6,6 @@
 class distance {
 	public int fun_distance(int x1, int y1, int x2, int y2){
 		// Your code goes here
-		return 0;
+		return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	}
 }
