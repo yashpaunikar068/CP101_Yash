82aaf3ce28b0314364f1d1cfa925883cf7ff7c38
yashpaunikar068 <ypaunikar@msitprogram.net>
2021-08-27 13:08:28 +0530

    solved

--- 02-queues-Java/Queue.java
+++ 02-queues-Java/Queue.java
@@ -1,22 +1,65 @@
-public class Queue{
-	int[] storage = new int[20];
-	int front;
-	int rear;
-	public Queue(int head){
-		front = 0;
-		rear = 0;
-		storage[rear++] = head;
-	}
-	
-	public void enqueue(int new_ele){
+class Queue {
+    int front, rear, size;
+    int capacity;
+    int array[];
+  
+    public Queue(int capacity)
+    {
+        this.capacity = capacity;
+        front = this.size = 0;
+        rear = capacity - 1;
+        array = new int[this.capacity];
+    }
+  
 
-	}
+    public boolean isFull(Queue queue)
+    {
+        return (queue.size == queue.capacity);
+    }
+  
 
-	public int peek(){
-		return 0;
-	}
+    public boolean isEmpty(Queue queue)
+    {
+        return (queue.size == 0);
+    }
+  
+    public void enqueue(int item)
+    {
+        if (isFull(this))
+            return;
+        this.rear = (this.rear + 1)
+                    % this.capacity;
+        this.array[this.rear] = item;
+        this.size = this.size + 1;
+        
+    }
 
-	public int dequeue(){
-		return 0;
-	}
+    public int dequeue()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+  
+        int item = this.array[this.front];
+        this.front = (this.front + 1)
+                     % this.capacity;
+        this.size = this.size - 1;
+        return item;
+    }
+
+    public int peek()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+  
+        return this.array[this.front];
+    }
+
+    public int rear()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+  
+        return this.array[this.rear];
+    }
 }
+  
