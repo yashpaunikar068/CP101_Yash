1a05bcf338019c1f8b0ee0e431e79ae41a0d8900
yashpaunikar068 <ypaunikar@msitprogram.net>
2021-09-01 02:36:53 +0530

    solved

--- 04-circlesintersect-Java/CirclesIntersect.java
+++ 04-circlesintersect-Java/CirclesIntersect.java
@@ -7,6 +7,13 @@
 public class CirclesIntersect {
 	public boolean circlesIntersect(int x1, int y1, int r1, int x2, int y2, int r2){
 		// Your code goes here....
-		return false;
+		int distSq = (x1 - x2) * (x1 - x2) +
+                     (y1 - y2) * (y1 - y2);
+        int radSumSq = (r1 + r2) * (r1 + r2);
+        if (distSq == radSumSq)
+            return true;
+        else if (distSq > radSumSq)
+            return false;
+		return true;
 	}
 }
