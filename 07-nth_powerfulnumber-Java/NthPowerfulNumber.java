61dfc08adb8d77114c437333ab175c3e8081cd4c
yashpaunikar068 <ypaunikar@msitprogram.net>
2021-09-01 04:53:59 +0530

    solved

--- 07-nth_powerfulnumber-Java/NthPowerfulNumber.java
+++ 07-nth_powerfulnumber-Java/NthPowerfulNumber.java
@@ -5,8 +5,36 @@
 // For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
 public class NthPowerfulNumber {
+	public boolean isPowerfulNumber(int n){
+		while (n % 2 == 0) {
+            int power = 0;
+            while (n % 2 == 0) {
+                n /= 2;
+                power++;
+            }
+            if (power == 1)
+                return false;
+        }
+
+        for (int factor = 3; factor <= Math.sqrt(n); factor += 2) {
+            int power = 0;
+            while (n % factor == 0) {
+                n = n / factor;
+                power++;
+            }
+            if (power == 1)
+                return false;
+        }
+        return (n == 1);
+	}
 	public long nthPowerfulNumber(int n) {
 		// Your code goes here
-		return -1;
+		int found = 1;
+		int guess = 0;
+		while (found <= n)
+			guess += 1;
+			if (isPowerfulNumber(guess))
+				found += 1;
+		return guess; 
 	}	
 }
