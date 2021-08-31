c68a9dc6150ff524fb4d71af5251c65afdd45ab0
yashpaunikar068 <ypaunikar@msitprogram.net>
2021-09-01 02:58:26 +0530

    solved

--- 03-applycaesarcipher-Java/CeaserCipher.java
+++ 03-applycaesarcipher-Java/CeaserCipher.java
@@ -10,9 +10,19 @@
 // assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
 
 public class CeaserCipher {
-	public String fun_applycaesarcipher(String msg, int shift){
-		// your code goes here
-		return "";
+	public String fun_applycaesarcipher(String message, int offset){
+		StringBuilder result = new StringBuilder();
+		for (char character : message.toCharArray()) {
+			if (character != ' ') {
+				int originalAlphabetPosition = character - 'a';
+				int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
+				char newCharacter = (char) ('a' + newAlphabetPosition);
+				result.append(newCharacter);
+			} else {
+				result.append(character);
+			}
+		}
+return result.toString();
 	}
 	
 }
