d27b464f04ee4698813aa256879457b2fdb2c565
yashpaunikar068 <ypaunikar@msitprogram.net>
2021-09-01 05:50:12 +0530

    solved

--- 02-binarysearch-Java/BinarySearch.java
+++ 02-binarysearch-Java/BinarySearch.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 class BinarySearch {
 	public int binary_search(int[] arr, int value){
         // Your code goes here
-        return -1;
+                Arrays.sort(arr);
+                return Arrays.binarySearch(arr, value) > 0 ? Arrays.binarySearch(arr, value) : -1;
 	}
 }
